ebd184b4b371ea1349e05ca804856761a19852de
Khushi <khushisrivastava53@gmail.com>
2020-06-30 14:46:00 +0530

    Created models in authentication app Created a User model using abstractBaseUser Created a country model which has country code and dial code

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import UserManager 
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField("Name", max_length=255, blank=True)
+    email = models.EmailField("Email", blank=True, unique=True)
+    phone = models.CharField("Phone", max_length=17, blank=False, unique=True, primary_key=True)
+    dp = models.FileField("Profile Picture", blank=True, editable=True)
+    is_verified = models.BooleanField("Is Verified", default=False)
+
+    USERNAME_FIELD = 'phone'
+    EMAIL_FIELD = 'email'
+
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return  self.name
+
+class Country(models.Model):
+    name = models.CharField("Country Name", max_length=255)
+    code = models.CharField("Country Code", max_length=255)
+    dial_code = models.CharField("Dial Code", max_length=10)
+
+    def __str__(self):
+        return self.dial_code
